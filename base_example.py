8b0a305d8044059a5a558297276a9a419f0aa6c5
Jordan Hurley <jordan.hurley@students.pcci.edu>
2025-04-10 14:14:14 -0500

    Example cleanup

--- base_example.py
+++ base_example.py
@@ -3,15 +3,16 @@ import sec
 from azure.devops.connection import Connection
 from msrest.authentication import BasicAuthentication
 
-connection = Connection(base_url="ado url", creds=BasicAuthentication(sec.username, sec.password))
+connection = Connection(base_url="https://dev.azure.com/example-organization", creds=BasicAuthentication(sec.username, sec.password))
 
-for repository_name in ["repo name", "repo name"]:
-    rc  = adoa.RepoClient("ado project name", connection, repository_name, "base branch", "working branch", "change title")
+for repository_name in ["repo-name1", "repo-name2"]:
+    rc  = adoa.RepoClient("example-project", connection, repository_name, "base-branch", "working-branch", "change title")
     
     # Make changes
-    content = rc.get_content("./file.example").replace("example1", "example2")
-    rc.edit("./file.example", content)
-    rc.add("/example.txt", "example content")
+    content = rc.get_content("./example1.txt").replace("example1", "example2")
+    rc.edit("./example1.txt", content)
+    rc.add("/example2.txt", "example content")
+    rc.delete("example3.txt")
 
     # Push changes and create merges
     rc.push_to_working()
