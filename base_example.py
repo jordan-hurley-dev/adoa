93f7dcb1070cd8dc3504525a04e26dc01bac85dc
jordan-hurley <jordanhurley@protonmail.com>
2025-10-21 15:51:41 -0500

    Updated base example

--- base_example.py
+++ base_example.py
@@ -6,7 +6,7 @@ from msrest.authentication import BasicAuthentication
 connection = Connection(base_url="https://dev.azure.com/example-organization", creds=BasicAuthentication(sec.username, sec.password))
 
 for repository_name in ["repo-name1", "repo-name2"]:
-    rc  = adoa.RepoClient("example-project", connection, repository_name, "base-branch", "working-branch", "change title")
+    rc  = adoa.RepoClient(connection, "example-project", repository_name, "base-branch", "working-branch", "change title")
     
     # Make changes
     content = rc.get_content("./example1.txt").replace("example1", "example2")
@@ -15,7 +15,7 @@ for repository_name in ["repo-name1", "repo-name2"]:
     rc.delete("example3.txt")
 
     # Push changes and create merges
-    rc.push_to_working()
+    rc.commit_to_working()
     # rc.pull_into("test")
     # rc.pull_into("preprod")
     # rc.pull_into("prod")
